c660b8e722e9f982502e325177f01b996514c639
parkash kumar <parkashmi66@gmail.com>
2024-04-10 23:30:37 +0530

    fix: fixed imports

--- src/app/(dashboard)/page.tsx
+++ src/app/(dashboard)/page.tsx
@@ -1,7 +1,5 @@
 'use client'
 import withAuth from "@/components/WithAuth";
-import { Topbar } from "@/components/organisms";
-import Image from "next/image";
 const Home=() =>{
   return (
     // <main className="flex min-h-screen flex-col items-center justify-between p-24">
