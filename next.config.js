deb89887bfd6faff8b1ac3fe534d84772d0d4c41
parkash kumar <parkashmi66@gmail.com>
2024-05-21 11:56:20 +0530

    config changed

--- next.config.js
+++ next.config.js
@@ -5,6 +5,7 @@ module.exports = {
       "usefuns.s3.ap-south-1.amazonaws.com",
       "usefuns-dev.s3.ap-south-1.amazonaws.com",
       'usefun.s3.ap-south-1.amazonaws.com',
+      'usefuns-bucket.blr1.digitaloceanspaces.com'
     ],
   },
 };
